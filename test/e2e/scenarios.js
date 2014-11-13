8414515c2d443d07fbfd924a8066a0f4a73c9a66
jo <jo@ubuntu14.04>
2014-11-13 15:53:47 +0900

    add e2e test.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,10 +2,20 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('PhoneCat App', function() {
+
+describe('Phone list view', function() {
 
   beforeEach(function() {
     browser.get('app/index.html');
   });
+
+
+  it('should list phones', function() {
+      var phonelist = element.all(by.repeater('phone in phonelist.phones'));
+      expect(phonelist.count()).toEqual(3);
+  });
+  
+});
 
 });
