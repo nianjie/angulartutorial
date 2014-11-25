9b7811a17cc2222305e33e19aa799758c3aa703e
jo <jo@ubuntu14.04>
2014-11-25 16:50:47 +0900

    inject $http service to controller.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,20 +3,10 @@
 /* Controllers */
 var app = angular.module('phonecatApp',[ ]);
 
-app.controller('PhoneListCtrl', function($scope) {
-    this.phones = [
-	{'name': 'Nexus S',
-	 'snippet': 'Fast just got faster with Nexus S.',
-	 'age':1
-	},
-	{'name': 'Motorola XOOM with Wi-Fi',
-	 'snippet': 'The Next, Next Generation tablet.',
-	 'age':2
-	},
-	{'name': 'Motorola XOOM ',
-	 'snippet': 'The Next, Next Generation tablet.',
-	 'age':3
-	}
-    ];
+app.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
+    var self = this;
+    $http.get('phones/phones.json').success(function(data) {
+	self.phones = data;
+    });
     this.orderProp = 'age';
-});
+}]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -11,17 +11,24 @@ describe('controllers', function() {
 });
 
 describe('PhoneListCtrl', function() {
-  var scope, ctrl;
+  var scope, ctrl, $httpBackend;
 
   beforeEach(module('phonecatApp'));
 
-  beforeEach(inject(function($controller) {
-      scope = {}, 
+  beforeEach(inject(function($controller, _$httpBackend_) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/phones.json').
+	  respond([{name:'Nexus S'}, {name:'Motorola DROID'}]);
+
+      scope = {};
       ctrl = $controller('PhoneListCtrl', {$scope:scope});
   }));
 
   it("should create 'phones' model with 3 phones", function() {
-      expect(ctrl.phones.length).toBe(3);
+      expect(ctrl.phones).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(ctrl.phones).toEqual([{name:'Nexus S'}, {name:'Motorola DROID'}]);
 
   });
 
