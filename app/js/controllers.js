ea93d53285ffc7a3ce014e8b5ae1da590f76d440
jo <jo@ubuntu14.04>
2014-12-01 17:04:35 +0900

    utilise multiple views with ng-route module.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,12 +1,19 @@
 'use strict';
 
 /* Controllers */
-var app = angular.module('phonecatApp',[ ]);
+var phonecatControllers = angular.module('phonecatControllers',[ ]);
 
-app.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+function($scope, $http) {
     var self = this;
     $http.get('phones/phones.json').success(function(data) {
 	self.phones = data;
     });
     this.orderProp = 'age';
+}]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+function($scope, $routeParams) {
+    var self = this;
+    this.phoneId = $routeParams.phoneId;
 }]);
