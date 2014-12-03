18e1ecd85c972a2ced32289ea23e75b67d67ae07
jo <jo@ubuntu14.04>
2014-12-03 17:30:14 +0900

    add a customised filter to convert boolean to glyphs.

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -4,4 +4,18 @@
 
 describe('filter', function() {
 
+    beforeEach(module('phonecatFilters'));
+
+    
+    describe('checkmark', function() {
+	var checkmark;
+	beforeEach(inject(function(checkmarkFilter) {
+	    checkmark = checkmarkFilter;
+	}));
+	
+	it('should convert boolean values to unicode checkmark or cross', function() {
+	    expect(checkmark(true)).toBe('\u2713');
+	    expect(checkmark(false)).toBe('\u2718');
+	});
+    });
 });
